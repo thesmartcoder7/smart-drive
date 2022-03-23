51282402d5074a8238fb9b45b9f19d2bb6f947a9
Samuel Martins <samuel.martins4.sm@gmail.com>
2022-03-23 12:21:51 +0300

    add user trip modal

--- js/index.js
+++ js/index.js
@@ -3,3 +3,45 @@ let timeline = new Date
 let currentYear = timeline.getFullYear()
 let footerYear = document.querySelector(".current-year")
 footerYear.textContent = currentYear
+
+/* - - - sample user object - - - */
+let samuel = {
+    username:"samuel",
+    password: "samuel"
+}
+
+/* - - - sample driver objects - - - */
+let noDisabilityD = [
+    {
+        name: "joshua kennedy",
+        rating: "4",
+        eta: 7,
+        inspection: true,
+        vehiclePlates: "SD123"
+    },
+    {
+        name: "timothy william",
+        rating: "3",
+        eta: 5,
+        inspection: true,
+        vehiclePlates: "SD456"
+    }
+]
+
+
+let disabilityD = [
+    {
+        name: "mathew amenya",
+        rating: "4",
+        eta: 7,
+        inspection: true,
+        vehiclePlates: "SD789"
+    },
+    {
+        name: "daniel makatu",
+        rating: "3",
+        eta: 5,
+        inspection: true,
+        vehiclePlates: "SD876"
+    }
+]
